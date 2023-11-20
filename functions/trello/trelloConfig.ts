af680808ee89991014bf4e306e31b193b188316c
Amer Slaiman <91747941+amermsl92@users.noreply.github.com>
2023-11-20 22:19:14 +0100

    pushed by mistake

--- functions/trello/trelloConfig.ts
+++
@@ -1,9 +0,0 @@
-// all the data in here are for testing
-export const trelloConfig = {
-  appName: "My personal Website",
-  appKey: "91cff7a008be931262e3fe57ca4b6c4e",//here where I user my key 
-  appSecret: "ff65897e805110ef65d7ed0e67345607aa2363535213b3f20ff62494c5d80665", //here where I user my secret 
-  callbackUrl: "http://localhost:3000/",
-};
-
-export default trelloConfig;
