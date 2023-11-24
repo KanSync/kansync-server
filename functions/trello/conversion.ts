5b7d6945edc279a3d80d925a0cf94d2b780d8728
gabrielfrimodig <gabrielfrimodig@gmail.com>
2023-11-24 17:33:44 +0100

    Revert "small changing as suggested"
    This reverts commit 2c8d2383578c6cf7c0974389f20d43c3406a5bc7.

--- functions/trello/conversion.ts
+++ functions/trello/conversion.ts
@@ -2,7 +2,7 @@ import { IUnifiedIssue, Assignee } from "../common";
 
 import { List } from "./trelloTypes";
 
-export function toUnifiedIssues(
+export function convertTrelloDataToUnifiedIssues(
   lists: List[],
 ): IUnifiedIssue[] {
   let unifiedIssues: IUnifiedIssue[] = [];
@@ -15,7 +15,7 @@ export function toUnifiedIssues(
       const unifiedIssue: IUnifiedIssue = {
         title: card.name || "Default Title",
         assignees: card.memberNames.map((name) => ({ name } as Assignee)),
-        author: { name: null },
+        author: { name: "Unknown" },
         body: card.desc,
         category: card.category,
         statusChangeTime: null,
@@ -24,7 +24,7 @@ export function toUnifiedIssues(
         lastEditedAt: card.dateLastActivity,
         projectID: card.projectID,
         dueDate: card.due || null,
-        labels: card.labels.map((label) => label.name),
+        labels: card.labels.map(label => label.name),
         dependencies: [],
       };
       unifiedIssues.push(unifiedIssue);
