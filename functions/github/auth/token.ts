0257e5278034422c00091f654bdd475f7d4f4987
Dan-cmyk <dante.grenholm.9198@student.uu.se>
2023-12-13 13:29:37 +0100

    Oauth for github init
    Co-authored-by: Linus Olofsson <56028244+Kax-y@users.noreply.github.com>

---
+++ functions/github/auth/token.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { Request, Response } from "express";
+import { OAuth2Client } from "@badgateway/oauth2-client";
+import auth from "../../oauth";
+
+
+const client = new OAuth2Client({
+    server: "https://github.com",
+    clientId: "e6afa30d9b65e51b2332",
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    authenticationMethod: "client_secret_post",
+    tokenEndpoint: "/login/oauth/access_token",
+    authorizationEndpoint: "/login/oauth/authorize",
+});
+
+
+export default async (req: Request, res: Response) => {
+    await auth(req, res, client)
+}
