deb06978bde5d598bd47d4cb1b9637724002f6c2
Kax-y <56028244+Kax-y@users.noreply.github.com>
2023-12-13 22:04:37 +0100

    Generalised the github API handler

--- functions/github/index.ts
+++ functions/github/index.ts
@@ -4,21 +4,24 @@ import { Request, Response } from "express";
 import { allowCors } from "../_utils/helpers";
 import { GET_PROJECT_CARDS } from "./schema/getProjectCards";
 import { toUnified } from "./conversion";
-import { IGithubIssue } from './interfaces';
+import { IGithubIssue } from "./interfaces";
 import { handleIssueRequest } from "../common";
 
 const handler = async (req: Request, res: Response) => {
-  if (!process.env.PERSONAL_ACCESS_TOKEN) {
-    res.status(500).send("Missing PERSONAL_ACCESS_TOKEN");
+  let reqAuthHeader = req.headers.authorization;
+
+  if (reqAuthHeader === undefined) {
+    res.status(403).send(`Missing authorization header.`);
     return;
   }
+
+  let [tokenType, token] = reqAuthHeader.split(" ");
 
   const repo = req.query.repo;
   const name = req.query.projectName;
 
-  if (repo === undefined || name === undefined
-  ) {
-    res.status(400).send(`Missing repository or project name.`);
+  if (token === undefined || repo === undefined || name === undefined) {
+    res.status(400).send(`Missing token, repository or project name.`);
     return;
   }
 
@@ -26,7 +29,7 @@ const handler = async (req: Request, res: Response) => {
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: `token ${process.env.PERSONAL_ACCESS_TOKEN}`,
+      Authorization: `${tokenType} ${token}`,
     },
     body: JSON.stringify({
       query: GET_PROJECT_CARDS,
@@ -38,23 +41,30 @@ const handler = async (req: Request, res: Response) => {
     method: "POST",
   }).then((response) => response.json());
 
-  let res_repo = result.data.search.nodes
+  if (result.data === undefined) {
+    res.status(403).send("Unauthorized.");
+    return;
+  }
+
+  let res_repo = result.data.search.nodes;
 
   if (res_repo.length == 0) {
     res.status(404).send("Unable to find repository.");
     return;
   }
 
-  let res_project = res_repo[0].projectsV2.nodes
+  let res_project = res_repo[0].projectsV2.nodes;
 
   if (res_project.length == 0) {
     res.status(404).send("Unable to find project.");
     return;
   }
-  
-  let issues = res_project[0].items.nodes.map((issue: IGithubIssue) => toUnified(issue))
 
-  res.status(200).send({ "num": issues.length, "issues": issues });
+  let issues = res_project[0].items.nodes.map((issue: IGithubIssue) =>
+    toUnified(issue),
+  );
+
+  res.status(200).send({ num: issues.length, issues: issues });
   return issues;
 };
 
