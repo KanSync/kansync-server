22faf0f5d2fe4b481ca081b4ed25ebc6e696c633
Kax-y <56028244+Kax-y@users.noreply.github.com>
2024-01-02 03:37:16 +0100

    Added error msg

--- functions/user/create.ts
+++ functions/user/create.ts
@@ -19,7 +19,7 @@ async function handler(req: Request, res: Response) {
 
     await createUser(login);
   } catch (error) {
-    res.status(500).send(`Database error occurred during user creation. ${error}`);
+    res.status(500).send(`Database error occurred during user creation. CWD: ${process.cwd()}\n ${error}`);
     return;
   }
   res.status(201).send("User created.");
