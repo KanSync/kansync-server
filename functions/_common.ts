65f7cc90cd908f774efb5a0f93480521a3e44730
Kax-y <56028244+Kax-y@users.noreply.github.com>
2024-01-02 04:02:04 +0100

    Give up on prisma

--- functions/_common.ts
+++ functions/_common.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import {
-  getIssuesFromDB,
-  projectExists,
-  storeIssuesToDB,
-  userExists,
-} from "./_database";
+// import {
+//   getIssuesFromDB,
+//   projectExists,
+//   storeIssuesToDB,
+//   userExists,
+// } from "./_database";
 import type { Issues } from "./_prisma/client";
 
 export interface Assignee {
@@ -41,47 +41,47 @@ export async function handleIssueRequest(
     return;
   }
 
-  user = user as string;
-  project_name = project_name as string;
+  // user = user as string;
+  // project_name = project_name as string;
 
-  try {
-    if (!userExists(user)) {
-      res.status(400).send("Invalid user.");
-      return;
-    }
-  } catch (error) {
-    res.status(500).send("Failed to validate user.");
-  }
+  // try {
+  //   if (!userExists(user)) {
+  //     res.status(400).send("Invalid user.");
+  //     return;
+  //   }
+  // } catch (error) {
+  //   res.status(500).send("Failed to validate user.");
+  // }
 
-  if (update || !(await projectExists(user, project_name))) {
-    let issues = await getIssuesFromBoard(req, res);
+  // if (update || !(await projectExists(user, project_name))) {
+  //   let issues = await getIssuesFromBoard(req, res);
 
-    // If issues is undefined getIssuesFromBoard should have sent an error response
-    if (issues !== undefined) {
-      try {
-        await storeIssuesToDB(user, project_name, issues);
-      } catch (error) {
-        // If the store fails ignore it as getIssuesFromBoard already consumes the response
-        console.log(error);
-      }
-    }
-    return;
-  }
+  //   // If issues is undefined getIssuesFromBoard should have sent an error response
+  //   if (issues !== undefined) {
+  //     try {
+  //       await storeIssuesToDB(user, project_name, issues);
+  //     } catch (error) {
+  //       // If the store fails ignore it as getIssuesFromBoard already consumes the response
+  //       console.log(error);
+  //     }
+  //   }
+  //   return;
+  // }
 
-  let issues: IUnifiedIssue[];
-  try {
-    issues = await getIssuesFromDB(user, project_name);
-  } catch (error) {
-    res
-      .status(error.status || 500)
-      .send(error.status ? error.msg : "Failed database get.");
-    return;
-  }
+  // let issues: IUnifiedIssue[];
+  // try {
+  //   issues = await getIssuesFromDB(user, project_name);
+  // } catch (error) {
+  //   res
+  //     .status(error.status || 500)
+  //     .send(error.status ? error.msg : "Failed database get.");
+  //   return;
+  // }
 
-  if (issues.length == 0) {
-    res.status(404).send("No issues linked to project.");
-    return;
-  }
+  // if (issues.length == 0) {
+  //   res.status(404).send("No issues linked to project.");
+  //   return;
+  // }
 
-  res.status(200).send({ num: issues.length, issues: issues });
+  // res.status(200).send({ num: issues.length, issues: issues });
 }
